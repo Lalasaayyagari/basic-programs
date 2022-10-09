582fa6b0bd1dfc410602c59b85a36bb5a8b0442b
Lalasaayyagari <101415779+Lalasaayyagari@users.noreply.github.com>
2022-10-09 19:36:43 +0530

    Update 4.6math_fun_pro.cpp

--- 4.6math_fun_pro.cpp
+++ 4.6math_fun_pro.cpp
@@ -12,3 +12,9 @@ int main(){
 	cout<<"\npow 2 to 3 is:"<<pow(2,3);
 	cout<<"\nlog(10) to base 10 is: "<<log10(10);
 }	
+/*sin(90)= 0.894
+cos(90)= -0.448
+tan(100)= -0.587
+square root of 9 is= 3.000
+pow 2 to 3 is:8.000
+log(10) to base 10 is: 1.000*/
